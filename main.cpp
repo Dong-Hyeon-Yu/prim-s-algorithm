91c00b51ef8edeff25fadd8c6879446b0e8b4957
Dong-Hyeon-Yu <ydh9516@gmail.com>
2021-11-25 15:27:52 +0900

    add comments for normal Heap generic class

--- main.cpp
+++ main.cpp
@@ -1,9 +1,8 @@
 #define _CRT_SECURE_NO_WARNINGS
 
-#include <cstdlib>
+#include <functional>  // for template struct less<_Ty>
 #include <cstdio>
 #include <vector>
-#include <algorithm>
 
 #define pii pair<int, int>
 #define vvp vector<vector<pii>> 
@@ -12,6 +11,7 @@
 #define vii vector<pii>
 using namespace std;
 
+// generic class Heap
 template <class _Ty, class _Compare = less<typename vector<_Ty>::value_type>>
 class Heap {
     using _Container        = vector<_Ty>;
@@ -19,76 +19,13 @@ class Heap {
     using _Iter             = typename _Container::iterator;
     using size_type         = typename _Container::size_type;
     using value_type        = typename _Container::value_type;
-private:
-    //int left_child_idx(int a) { return (a << 1) + 1; }
-    //int right_child_idx(int a) { return (a << 1) + 2; }
-    //_Iter left_child(int a) {return c.begin() + left_child_idx(a);}
-    //_Iter right_child(int a) {return c.begin() + right_child_idx(a);}
 
+private:
     int left_child_idx(_Iter a) { return (distance(c.begin(), a) << 1) + 1; }
     int right_child_idx(_Iter a) { return (distance(c.begin(), a) << 1) + 2; }
-    _Iter left_child(_Iter a) { return c.begin() + ((distance(c.begin(), a) << 1) + 1);}
-    _Iter right_child(_Iter a) {return c.begin() + ((distance(c.begin(), a) << 1) + 2);}
+    _Iter left_child(_Iter a) { return c.begin() + ((distance(c.begin(), a) << 1) + 1); }
+    _Iter right_child(_Iter a) { return c.begin() + ((distance(c.begin(), a) << 1) + 2); }
 
-
-	//int _promote(int _hStop, int vacant, int h) {
-	//	/*
-	//	go down towart bigger child each step from _hStop to h.
- //       */
- //       int _finalStop_idx; // 내려갈 위치
- //       if (h <= _hStop) { // 내려갈만큼 내려갔을경우 현재위치를 리턴시킴
- //           _finalStop_idx = vacant;
- //       }
- //       else if (comp(*right_child(vacant),*left_child(vacant)) && right_child_idx(vacant) < c.size()) { // 우측이 더 작을 경
- //           iter_swap(c.begin() + vacant, right_child(vacant));
- //           _finalStop_idx = _promote(_hStop, right_child_idx(vacant), h - 1);
- //       }
- //       else{ //좌측 자식
- //           iter_swap(c.begin() + vacant, left_child(vacant));
- //           _finalStop_idx = _promote(_hStop, left_child_idx(vacant), h - 1);
- //       }
- //       return _finalStop_idx;
-	//}
-
-	//void _fixHeapFast(value_type K, int vacant,  int _h) {
- //       if (_h == 0) return;
- //       if (_h == 1) { // h가 1이하일경우
- //           if (left_child_idx(vacant) >= c.size())
- //               return; // h가 1이남았지만 해당노드의 자식이 없을수도있다.
- //           else if (comp(*right_child(vacant), K) && right_child_idx(vacant) < c.size()){
- //               _Iter smaller = (comp(*left_child(vacant), *right_child(vacant)) ? left_child(vacant) : right_child(vacant));
- //               iter_swap(c.begin() + vacant, smaller);
- //           }
- //           else if (comp(*left_child(vacant), K) && left_child_idx(vacant) < c.size()) {
- //               iter_swap(c.begin() + vacant, left_child(vacant));
- //           }
- //           return;
- //       }
-
- //       int hStop = _h / 2;
- //       int vacStop = _promote(hStop, vacant, _h); // 새로운 위치
- //       int vacParent = (vacStop-1) >> 1; // 새로 찾은 위치의 부모
- //       if (comp(K, *(c.begin() + vacParent))) {
- //           iter_swap(c.begin() + vacStop, c.begin() + vacParent);
- //           _bubbleUpHeap(vacant, K, vacParent);
- //       }
- //       else
- //           _fixHeapFast(K, vacStop,  hStop);
- //   }
- //   void _bubbleUpHeap(int root, value_type K, int vacant){
- //       if (vacant == root)
- //           *(c.begin() + vacant) = K;
- //       else {
- //           int parent = (vacant-1)>>1;
- //           if (comp(*(c.begin() + parent), K)) { // 부모 보다 크다면 자기위치를 찾은것
- //               *(c.begin() + vacant) = K;
- //           }
- //           else { // 부모 보다 작다면 버블업힙을 계속 실행
- //               iter_swap(c.begin() + vacant, c.begin() + parent);
- //               _bubbleUpHeap(root, K, parent);
- //           }
- //       }
- //   }
 	void _bubbleUpHeap(value_type _Value, int vacant) {
         /*
          * repeat bubble up until there is not min-heap violation
@@ -98,23 +35,27 @@ private:
         int _hole_idx = vacant;
         for(
                 int _hole_parent_idx = (_hole_idx - 1) >> 1;
-                _hole_idx!=0 && comp(_Value, *(c.begin() + _hole_parent_idx));
+                _hole_idx!=0 && comp(_Value, *(c.begin() + _hole_parent_idx));  /* Until Not violation */
                 _hole_parent_idx = (_hole_idx - 1) >> 1
                 ){
-            *(c.begin() + _hole_idx) = *(c.begin() + _hole_parent_idx);/* up hole and down value */
+            *(c.begin() + _hole_idx) = *(c.begin() + _hole_parent_idx); /* up hole and down value */
             _hole_idx  = _hole_parent_idx;
         }
 
         *(c.begin() + _hole_idx) = _Value;
     }
     void _downHeap(_Iter root) {
+        /*
+        * repeat downHeap until there is not min-heap violation
+        */
         _Iter smaller;
-        if (left_child_idx(root) >= c.size()) return;
-        else if (right_child_idx(root) >= c.size()) smaller = left_child(root);
-        else smaller = comp(*left_child(root), *right_child(root)) ? left_child(root) : right_child(root);
+        if (left_child_idx(root) >= c.size()) return; /* leaf node! */
+        else if (right_child_idx(root) >= c.size()) smaller = left_child(root); /* only one child */
+        else smaller = comp(*left_child(root), *right_child(root)) ? left_child(root) : right_child(root); /* have two children */
 
-        if (!comp(*root, *smaller)) {
-            iter_swap(root, smaller);
+        /* heap violation !! */
+        if (!comp(*root, *smaller)) { 
+            swap(*root, *smaller);
             _downHeap(smaller);
         }
     }
@@ -122,21 +63,16 @@ public:
 	explicit Heap(const _Compare& _Pred) : c(), comp(_Pred) {}
     Heap() = default;;
 
-    size_type size() const { return c.size(); }
-    bool empty() const { return c.empty(); }
-	const_reference top() const {
-        /* require none-empty! */
-        return c.front();
+	const_reference top() const { 
+        return c.front(); 
     }
 	void push(const _Ty& e) {
         c.push_back(e);
         _bubbleUpHeap(e, c.size()-1);
     }
 	void pop() {
-        if (c.empty()) return;
         *(c.begin()) = c.back();
         c.pop_back();
-        //_fixHeapFast(c.front(), 0, static_cast<int>(log(c.size())+1));
         _downHeap(c.begin());
     }
 protected:
@@ -159,12 +95,12 @@ void prim(int start, const vvp& edge_list, vb& visited, int n) {
 	tree.push_back(start);
 	int curV = start;
 
-	while (--n) { // n-1 번 반
-		//  인접 정점을 fringe Set 에 추
+	while (--n) { 
+		//  add adjacent vertex to fringe set
 		for (auto& adj : edge_list[curV]) 
 			if (!visited[adj.first]) fringe.push(adj);
 
-        // min of fringe Set
+        // find minimal cost vertex in fringe set
         int target, cur_cost;
         do {
             target = fringe.top().first;
@@ -172,7 +108,7 @@ void prim(int start, const vvp& edge_list, vb& visited, int n) {
             fringe.pop();
         } while(visited[target]);
 
-		//
+		// set visited & move into tree
 		visited[target] = true;
 		tree.push_back(target);
 		total_dist += cur_cost;
@@ -185,47 +121,24 @@ void prim(int start, const vvp& edge_list, vb& visited, int n) {
 
 int main() {
 	int n, m, q;
-	if (scanf("%d%d%d", &n, &m, &q) != 3) {
-		printf("error scanf!\n");
-		return 1;
-	};
+    scanf("%d%d%d", &n, &m, &q);
 
-	vvp data(n + 1); // edge list
+	vvp data(n + 1); // adjacent list
 	vb visited(n + 1, false);
 
 	int s, e, c;
 	while (m--) {
-		if (scanf("%d%d%d", &s, &e, &c) != 3) {
-			printf("error scanf during get data!\n");
-			return 1;
-		};
+        scanf("%d%d%d", &s, &e, &c);
 		data[s].push_back({ e, c });
 		data[e].push_back({ s, c });
 	}
 
 	while (q--) {
 		int startV = 0;
-		if (scanf("\nP %d", &startV) != 1) {
-			printf("error scanf during get start vertex!\n");
-			return 1;
-		};
+        scanf("\nP %d", &startV);
 		prim(startV, data, visited, n);
 		fill(visited.begin(), visited.end(), false);  // reset dist vector for next time
 	}
 
 	return 0;
 }
-
-/* for local debugging */
-//int _main() {
-//    int n;
-//    Heap<int> pq;
-//    while(scanf("%d", &n)){
-//        if(n>0) pq.push(n);
-//        else{
-//            printf("top: %d\n", pq.top());
-//            pq.pop();
-//        }
-//
-//    }
-//}
